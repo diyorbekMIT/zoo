6ae8c4383aa838c6acb75609270b8e21d065d6fa
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-08-14 19:55:24 +0900

    feat introduction to ZOO

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { DogService } from './dog.service';
+import { query } from 'express';
+
+@Controller('dog')
+export class DogController {
+    constructor(private readonly dogService: DogService) {}
+    
+        @Get()
+        public getHello(): string {
+            return this.dogService.getHello();   
+        }
+
+        @Get("greet/:id")
+        public introduce(@Param() params: any, @Query() query: any): string {
+            console.log("params: ", params);
+            console.log("query: ", query);
+            return this.dogService.introduce();   
+        }
+
+        @Post("edit")
+        public modifyDetail(@Body() body: any): string {
+            console.log("body: ", body);
+            return this.dogService.modifyDetail();   
+        }
+    }
+    
+
